675e8a98900611c6c910743888008913342af74c
cinfacol <cinfacol@gmail.com>
2023-11-25 13:25:56 -0500

    nextconfig button import

--- ecommerce-store/app/(routes)/page.tsx
+++ ecommerce-store/app/(routes)/page.tsx
@@ -8,7 +8,7 @@ export const revalidate = 0;
 
 const HomePage = async () => {
   const products = await getProducts({ isFeatured: true });
-  const billboard = await getBillboard("0e12e5cf-29ab-4529-b8d5-c5371dae1f7b");
+  const billboard = await getBillboard("c7994397-84ed-4198-b036-dfa0478305d5");
 
   return (
     <Container>
